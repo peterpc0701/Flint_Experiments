6b6f4e2ce29923c4594df10ff1c18325e42638d7
peterpc0701 <peicheng89@gmail.com>
2015-06-11 20:01:57 +0800

    add the LR experiment !

---
+++ LRTest.scala
@@ -0,0 +1,160 @@
+package cn.edu.hust
+
+/**
+ * Created by peicheng on 15-6-11.
+ */
+
+import org.apache.spark._
+
+import java.util.Random
+
+import org.apache.spark.rdd.RDD
+
+import scala.math.exp
+
+import breeze.linalg.{Vector, DenseVector}
+
+import java.io.{DataOutputStream, ByteArrayOutputStream}
+
+import org.apache.hadoop.io.WritableComparator
+
+class PointChunk(dimensions: Int,size: Int = 4196) extends ByteArrayOutputStream(size) { self =>
+
+  def getVectorValueIterator(w: Array[Double]) = new Iterator[Array[Double]] {
+    var offset = 0
+    var currentPoint=new Array[Double](dimensions)
+    var i = 0
+    var y = 0.0
+    var dotvalue = 0.0
+
+    override def hasNext = offset < self.count
+
+    override def next() = {
+      if (!hasNext) Iterator.empty.next()
+      else {
+        //read data from the chunk
+        i=0
+        while (i < dimensions) {
+          currentPoint(i)= WritableComparator.readDouble(buf, offset)
+          offset += 8
+          i += 1
+        }
+        y = WritableComparator.readDouble(buf, offset)
+        offset += 8
+        //calculate the dot value
+        i=0
+        dotvalue = 0.0
+        while (i < dimensions) {
+          dotvalue += w(i)*currentPoint(i)
+          i += 1
+        }
+        //transform to values
+        i=0
+        while (i < dimensions) {
+          currentPoint(i) *= (1 / (1 + exp(-y * dotvalue)) - 1) * y
+          i += 1
+        }
+        currentPoint.clone()
+      }
+    }
+  }
+}
+/**
+ * Logistic regression based classification.
+ * Usage: SparkLR [slices]
+ *
+ * This is an example implementation for learning how to use Spark. For more conventional use,
+ * please refer to either org.apache.spark.mllib.classification.LogisticRegressionWithSGD or
+ * org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS based on your needs.
+ */
+
+object LRTest {
+  val N = 1000  // Number of data points
+  val D = 100   // Numer of dimensions
+  val R = 0.7  // Scaling factor
+  val ITERATIONS = 5
+  val rand = new Random(42)
+
+  case class DataPoint(x: Vector[Double], y: Double)
+
+  // Initialize w to a random value
+  var w = DenseVector.fill(D){2 * rand.nextDouble - 1}
+  println("Initial w: " + w)
+
+  def generateData(size: Int) = {
+    def generatePoint(i: Int) = {
+      val y = if(i % 2 == 0) -1 else 1
+      val x = DenseVector.fill(D){rand.nextGaussian + y * R}
+      DataPoint(x, y)
+    }
+    Array.tabulate(size)(generatePoint)
+  }
+
+  def testNative(points: RDD[DataPoint]): Unit = {
+    points.cache()
+    val startTime = System.currentTimeMillis
+    for (i <- 1 to ITERATIONS) {
+      println("On iteration " + i)
+      val gradient = points.map { p =>
+        p.x * (1 / (1 + exp(-p.y * (w.dot(p.x)))) - 1) * p.y
+      }.reduce(_ + _)
+      w -= gradient
+    }
+    val duration = System.currentTimeMillis - startTime
+    println("Duration is " + duration / 1000.0 + " seconds")
+    // println("Final w: " + w.length)
+    //println("Final w: " + w)
+  }
+
+  def testOptimized(points: RDD[DataPoint]): Unit = {
+    val cachedPoints = points.mapPartitions { iter =>
+      val chunk = new PointChunk(D)
+      val dos = new DataOutputStream(chunk)
+      for (point <- iter) {
+        point.x.foreach(dos.writeDouble)
+        dos.writeDouble(point.y)
+      }
+      Iterator(chunk)
+    }.cache()
+
+    val w_op=new Array[Double](D)
+    for(i <- 0 to D-1)
+      w_op(i) = w(i)
+
+    val startTime = System.currentTimeMillis
+    for (i <- 1 to ITERATIONS) {
+      println("On iteration " + i)
+      val gradient= cachedPoints.mapPartitions{ iter =>
+        val chunk = iter.next()
+        chunk.getVectorValueIterator(w_op)
+      }.reduce{(lArray, rArray) =>
+        val result_array=new Array[Double](lArray.length)
+        for(i <- 0 to D-1)
+          result_array(i) = lArray(i) + rArray(i)
+        result_array
+      }
+
+      for(i <- 0 to D-1)
+        w_op(i) = w_op(i) - gradient(i)
+    }
+    val duration = System.currentTimeMillis - startTime
+    println("Duration is " + duration / 1000.0 + " seconds")
+
+  }
+
+  def main(args: Array[String]) {
+
+    val sparkConf = new SparkConf().setAppName("SparkLR").setMaster("local")
+    val sc = new SparkContext(sparkConf)
+    val numSlices = if (args.length > 0) args(0).toInt else 2
+    val points = sc.parallelize(generateData(N), numSlices)
+
+    //test the original version
+    //testNative(points)
+
+    //test the manual version
+    testOptimized(points)
+
+    sc.stop()
+  }
+}
---
+++ PRTest.scala
@@ -0,0 +1,203 @@
+package cn.edu.hust
+
+import java.io.{DataOutputStream, ByteArrayOutputStream}
+
+import org.apache.hadoop.io.WritableComparator
+import org.apache.log4j._
+import org.apache.spark._
+import org.apache.spark.rdd.{RDD, ShuffledRDD}
+ 
+import SparkContext._
+import org.apache.spark.storage.StorageLevel
+
+class EdgeChunk(size: Int = 4196) extends ByteArrayOutputStream(size) { self =>
+
+  def getInitValueIterator(value: Float) = new Iterator[(Int, Float)] {
+    var offset = 0
+    
+    override def hasNext = offset < self.count
+    
+    override def next() = {
+      if (!hasNext) Iterator.empty.next()
+      else {
+        val srcId = WritableComparator.readInt(buf, offset)
+        offset += 4
+        val numDests = WritableComparator.readInt(buf, offset)
+        offset += 4 + 4 * numDests
+        (srcId, value)
+      }
+    }
+  }
+  
+  def getMessageIterator(vertices: Iterator[(Int, Float)]) = new Iterator[(Int, Float)] {
+    var changeVertex = true
+    var offset = 0
+    var currentDestIndex = 0
+    var currentDestNum = 0
+    var currentContrib = 0.0f
+
+    override def hasNext = !changeVertex || vertices.hasNext
+
+    override def next() = {
+      if (!hasNext) Iterator.empty.next()
+      else {
+        if (changeVertex) {
+          val currentVertex = vertices.next()
+          while (currentVertex._1 != WritableComparator.readInt(buf, offset)) {
+            offset += 4
+            val numDests = WritableComparator.readInt(buf, offset)
+            offset += 4 + 4 * numDests
+          }
+          offset += 4
+          currentDestNum = WritableComparator.readInt(buf, offset)
+          offset += 4
+          currentDestIndex = 0
+          currentContrib = currentVertex._2 / currentDestNum
+          changeVertex = false
+        }
+
+        currentDestIndex += 1
+        if (currentDestIndex == currentDestNum) changeVertex = true
+
+        val destId = WritableComparator.readInt(buf, offset)
+        offset += 4
+        
+        (destId, currentContrib)
+      }
+    }
+    
+  }
+
+  def getMessageIterator2(vertices: Iterator[(Long, Double)]) = new Iterator[(Long, Double)] {
+    var changeVertex = true
+    var offset = 0
+    var currentDestIndex = 0
+    var currentDestNum = 0
+    var currentContrib = 0.0
+
+    override def hasNext =  {
+      if (offset >= self.count) {
+        while (vertices.hasNext)
+          vertices.next()
+      }
+      offset < self.count
+    }
+
+    override def next() = {
+      if (!hasNext) Iterator.empty.next()
+      else {
+        if (changeVertex) {
+          WritableComparator.readLong(buf, offset)
+          offset += 8
+          currentDestNum = WritableComparator.readInt(buf, offset)
+          offset += 4
+          currentDestIndex = 0
+          currentContrib = 1.0 / currentDestNum
+          changeVertex = false
+        }
+
+        currentDestIndex += 1
+        if (currentDestIndex == currentDestNum) changeVertex = true
+
+        val destId = WritableComparator.readLong(buf, offset)
+        offset += 8
+
+        (destId, currentContrib)
+      }
+    }
+
+  }
+  
+}
+
+object PRTest {
+  private val ordering = implicitly[Ordering[Int]]
+  private val iters = 3
+  
+  def testNative(links: RDD[(Int, Iterable[Int])]) {
+    links.persist(StorageLevel.MEMORY_ONLY).foreach(_ => Unit)
+
+    val initRanks = links.mapValues(v => 1.0f)
+
+    var ranks = initRanks
+    
+    val startTime = System.currentTimeMillis
+    for (i <- 1 to iters) {
+      val contribs = links.join(ranks, links.partitioner.get).values.flatMap { case (urls, rank) =>
+        val contrib = rank / urls.size
+        urls.map(url => (url, contrib))
+      }
+      ranks = contribs.reduceByKey(links.partitioner.get, _ + _).mapValues(0.15f + 0.85f * _)
+    }
+ //   ranks.foreach(_ => Unit)
+    val duration = System.currentTimeMillis - startTime
+    println("Duration is " + duration / 1000.0 + " seconds")
+    
+  }
+  
+  def testOptimized(groupedEdges: RDD[(Int, Iterable[Int])]) {
+    val cachedEdges = groupedEdges.mapPartitions ( iter => {
+      val chunk = new EdgeChunk
+      val dos = new DataOutputStream(chunk)
+      for ((src, dests) <- iter) {
+        dos.writeInt(src)
+        dos.writeInt(dests.size)
+        dests.foreach(dos.writeInt)
+      }
+      Iterator(chunk)
+    }, true).cache()
+
+  //  cachedEdges.foreach(_ => Unit)
+    
+    val initRanks = cachedEdges.mapPartitions( iter => {
+      val chunk = iter.next()
+      chunk.getInitValueIterator(1.0f)
+    }, true)
+
+    var ranks = initRanks
+    
+    val startTime = System.currentTimeMillis
+    for (i <- 1 to iters) {
+      val contribs = cachedEdges.zipPartitions(ranks) { (EIter, VIter) =>
+        val chunk = EIter.next()
+        chunk.getMessageIterator(VIter)
+      }
+      ranks = contribs.reduceByKey(cachedEdges.partitioner.get, _ + _).asInstanceOf[ShuffledRDD[Int, _, _]].
+        setKeyOrdering(ordering).
+        asInstanceOf[RDD[(Int, Float)]].
+        mapValues(0.15f + 0.85f * _)
+    }
+ //   ranks.foreach(_ => Unit)
+    val duration = System.currentTimeMillis - startTime
+    println("Duration is " + duration / 1000.0 + " seconds")
+        
+  }
+  
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("Spark PageRank Test").
+      setMaster("local").
+      set("spark.shuffle.memoryFraction", "0.6")
+    val spark = new SparkContext(conf)
+
+    //Logger.getRootLogger.setLevel(Level.FATAL)
+
+    val slices = 2
+    val n = 120000 * slices
+    val edgesPerVert = 50
+    val edges = spark.parallelize(0 until n, slices).flatMap ( x => {
+      val result = new Array[(Int, Int)](edgesPerVert)
+      for (i <- 0 until edgesPerVert) {
+        result(i) = (x, (x + slices + i) % n)
+      }
+      result
+    }).groupByKey().
+      asInstanceOf[ShuffledRDD[Int, _, _]].
+      setKeyOrdering(ordering).
+      asInstanceOf[RDD[(Int, Iterable[Int])]]
+
+    //testNative(edges)
+    testOptimized(edges)
+    
+    spark.stop()
+  }
+}
